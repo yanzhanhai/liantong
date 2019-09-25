183e131d0f9d3d29692700ae9272ca5bf758d1e3
yanzhanhai <yzh031300@sina.com>
2019-09-25 16:58:53 +0800

    git测试2

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -2,10 +2,14 @@ package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
